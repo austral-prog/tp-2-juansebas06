f8bad0e570239c5fc19dba1eabbfccb155a9afb1
juansebas06 <162723503+juansebas06@users.noreply.github.com>
2024-04-04 21:53:37 -0600

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,13 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    first_name = "AdA"
+    last_name = "LoVeLAce"
+    frase = f"{first_name.lower()} {last_name.lower()}"
+    print(frase)
+    frase_1 = f"{first_name.title()} {last_name.title()}"
+    print(frase_1)
+    frase_2 = f"{first_name.upper()} {last_name.upper()}"
+    print(frase_2)
+    frase_3 = "   "f"{first_name.lower()} {last_name.lower()}"
+    print("     "+ frase_3)
